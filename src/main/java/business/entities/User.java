d5a2a7c0a848cd2c9fa8b80180f18b1666027cd1
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-04-19 09:23:46 +0200

    fix database connection

--- src/main/java/business/entities/User.java
+++ src/main/java/business/entities/User.java
@@ -1,57 +1,47 @@
 package business.entities;
 
-public class User
-{
+public class User {
 
-    public User(String email, String password, String role)
-    {
+    public User(String email, String password, String role) {
         this.email = email;
         this.password = password;
         this.role = role;
     }
 
-    private int id; // just used to demo retrieval of autogen keys in UserMapper
+    private int id;
     private String email;
-    private String password; // Should be hashed and secured
+    private String password;
     private String role;
 
-    public String getEmail()
-    {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email)
-    {
+    public void setEmail(String email) {
         this.email = email;
     }
 
-    public String getPassword()
-    {
+    public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password)
-    {
+    public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getRole()
-    {
+    public String getRole() {
         return role;
     }
 
-    public void setRole(String role)
-    {
+    public void setRole(String role) {
         this.role = role;
     }
 
-    public int getId()
-    {
+    public int getId() {
         return id;
     }
 
-    public void setId(int id)
-    {
+    public void setId(int id) {
         this.id = id;
     }
 
--- src/main/java/web/FrontController.java
+++ src/main/java/web/FrontController.java
@@ -17,9 +17,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "FrontController", urlPatterns = {"/fc/*"})
 public class FrontController extends HttpServlet
 {
-    private final static String USER = "dev";
-    private final static String PASSWORD = "ax2";
-    private final static String URL = "jdbc:mysql://localhost:3306/startcode?serverTimezone=CET";
+    private final static String USER = "root";
+    private final static String PASSWORD = "root123";
+    private final static String URL = "jdbc:mysql://localhost:3306/cupcake?serverTimezone=CET";
 
     public static Database database;
 
