d5f677bc37ba2ff3b99179fdfdbfa903ebb33eb5
FrederikBA <54848880+FrederikBA@users.noreply.github.com>
2021-04-27 17:00:05 +0200

    final release

--- src/test/java/business/persistence/UserMapperTest.java
+++ src/test/java/business/persistence/UserMapperTest.java
@@ -5,10 +5,12 @@ import business.exceptions.UserException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.sql.SQLException;
 import java.sql.Statement;
+
 import static org.junit.jupiter.api.Assertions.*;
-/*
+
 public class UserMapperTest {
 
     private final static String DATABASE = "startcode";  // Change this to your own database
@@ -33,17 +35,17 @@ public class UserMapperTest {
     @BeforeEach
     public void setUp() {
 
-            // reset test database
-            try ( Statement stmt = database.connect().createStatement() ) {
-                stmt.execute("drop table if exists users" );
-                stmt.execute("create table " + TESTDATABASE + ".users LIKE " + DATABASE + ".users;" );
-                stmt.execute(
+        // reset test database
+        try (Statement stmt = database.connect().createStatement()) {
+            stmt.execute("drop table if exists users");
+            stmt.execute("create table " + TESTDATABASE + ".users LIKE " + DATABASE + ".users;");
+            stmt.execute(
                     "insert into users values " +
-                    "(1,'jens@somewhere.com','jensen','customer'), " +
-                    "(2,'ken@somewhere.com','kensen','customer'), " +
-                    "(3,'robin@somewhere.com','batman','employee')");
-            } catch (SQLException ex) {
-            System.out.println( "Could not open connection to database: " + ex.getMessage() );
+                            "(1,'jens@somewhere.com','jensen','customer'), " +
+                            "(2,'ken@somewhere.com','kensen','customer'), " +
+                            "(3,'robin@somewhere.com','batman','employee')");
+        } catch (SQLException ex) {
+            System.out.println("Could not open connection to database: " + ex.getMessage());
         }
     }
 
@@ -56,32 +58,34 @@ public class UserMapperTest {
     @Test
     public void testLogin01() throws UserException {
         // Can we log in
-        User user = userMapper.login( "jens@somewhere.com", "jensen" );
-        assertTrue( user != null );
+        User user = userMapper.login("jens@somewhere.com", "jensen");
+        assertTrue(user != null);
     }
 
     @Test
     public void testLogin02() throws UserException {
         // We should get an exception if we use the wrong password
         assertThrows(UserException.class, () ->
-            {User user = userMapper.login( "jens@somewhere.com", "larsen" ); });
+        {
+            User user = userMapper.login("jens@somewhere.com", "larsen");
+        });
 
     }
 
     @Test
     public void testLogin03() throws UserException {
         // Jens is supposed to be a customer
-        User user = userMapper.login( "jens@somewhere.com", "jensen" );
-        assertEquals( "customer", user.getRole() );
+        User user = userMapper.login("jens@somewhere.com", "jensen");
+        assertEquals("customer", user.getRole());
     }
 
     @Test
     public void testCreateUser01() throws UserException {
         // Can we create a new user - Notice, if login fails, this will fail
         // but so would login01, so this is OK
-        User original = new User( "king@kong.com", "uhahvorhemmeligt", "konge" );
-        userMapper.createUser( original );
-        User retrieved = userMapper.login( "king@kong.com", "uhahvorhemmeligt" );
-        assertEquals( "konge", retrieved.getRole() );
+        User original = new User("king@kong.com", "uhahvorhemmeligt", "konge");
+        userMapper.createUser(original);
+        User retrieved = userMapper.login("king@kong.com", "uhahvorhemmeligt");
+        assertEquals("konge", retrieved.getRole());
     }
-}*/
+}
